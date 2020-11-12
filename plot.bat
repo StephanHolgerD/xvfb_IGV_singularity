bb28a4ba64fc37fc5f4574b2eaf48c3d2d4944b7
StephanHolgerD <stephan.drukewitz@uni-leipzig.de>
2020-11-12 09:34:43 +0100

    .bat file which is provided to igv

---
+++ plot.bat
@@ -0,0 +1,25 @@
+new
+maxPanelHeight 5000
+genome hg19
+snapshotDirectory Snapshots
+load /mnt/pat2.bam
+group strand
+goto chr2:41196281-41196341
+sort base
+collapse
+snapshot Patient2_pat2_brcaBlaBlub_pat2_out_pad30_collapse.png
+expand
+snapshot Patient2_pat2_brcaBlaBlub_pat2_out_pad30_expand.png
+goto chr2:41196236-41196386
+sort base
+collapse
+snapshot Patient2_pat2_brcaBlaBlub_pat2_out_pad75_collapse.png
+expand
+snapshot Patient2_pat2_brcaBlaBlub_pat2_out_pad75_expand.png
+goto chr2:41196161-41196461
+sort base
+collapse
+snapshot Patient2_pat2_brcaBlaBlub_pat2_out_pad150_collapse.png
+expand
+snapshot Patient2_pat2_brcaBlaBlub_pat2_out_pad150_expand.png
+exit
